a3ca24b796f2d3a1f532ebaa072d48265aabfd15
André Louzada <andretavareslouzada@gmail.com>
2026-02-12 17:08:31 -0300

    criacao de Controllers, MontarPedido, e itemPedidoDTO

---
+++ trabalho-delivery/src/main/java/com/mycompany/trabalho/delivery/aplicacao/useCases/MontarPedido.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.trabalho.delivery.aplicacao.useCases;
+
+//import com.mycompany.trabalho.delivery.dominio.adapter.ConsoleLogAdapter;
+//import com.mycompany.trabalho.delivery.dominio.model.pedido.Pedido;
+//import com.mycompany.trabalho.delivery.dominio.model.pedido.PedidoPendenteState;
+//import com.mycompany.trabalho.delivery.dominio.port.IClienteRepository;
+//import com.mycompany.trabalho.delivery.dominio.port.IPedidoRepository;
+//import com.mycompany.trabalho.delivery.aplicacao.dto.ItemPedidoDTO;
+//import com.mycompany.trabalho.delivery.dominio.model.cliente.Cliente;
+
+/**
+ *
+ * @author André
+ */
+public class MontarPedido {
+//    private final IPedidoRepository repositorioPedidos;
+//    private final IClienteRepository repositorioClientes;
+//    private final Cliente cliente;
+//    
+//
+//    public MontarPedido(IPedidoRepository repositorioPedidos, IClienteRepository repositorioClientes) {
+//        this.repositorioPedidos = repositorioPedidos;
+//       this.repositorioClientes = repositorioClientes;
+//    }
+//    
+//    public void executar(String cpf, List<ItemPedidoDTO> itens) {
+//        // TO DO verificar essa instancia concreta de ConsoleLogAdapter()
+//        //  verificar se pedido nao precisa ter um identificador e como fazer isso
+//        Cliente cliente = repositorioClientes.buscarClientePorCPF(this.cpf);
+//            if(cliente == null){
+//                repositorioClientes.salvarCliente(cliente);
+//            }
+//        }
+//        Pedido pedido = new Pedido(new ConsoleLogAdapter(), cpf, itens, new PedidoPendenteState());
+//        repositorioPedidos.salvarPedido(pedido);
+//        
+//        //System.out.println("Pedido " + pedido. + " foi criado com sucesso.");
+//    }
+}
