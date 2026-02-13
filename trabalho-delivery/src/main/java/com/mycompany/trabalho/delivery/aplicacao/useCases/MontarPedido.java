28542d678b23521a92c3df2284c32a05ca8dc928
André Louzada <andretavareslouzada@gmail.com>
2026-02-12 22:16:00 -0300

    criacao de Controllers, MontarPedido, e itemPedidoDTO (refazendo commit pois foi sobrecristo sem os trechos alterados)

--- trabalho-delivery/src/main/java/com/mycompany/trabalho/delivery/aplicacao/useCases/MontarPedido.java
+++ trabalho-delivery/src/main/java/com/mycompany/trabalho/delivery/aplicacao/useCases/MontarPedido.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.trabalho.delivery.aplicacao.useCases;
-
+//
 //import com.mycompany.trabalho.delivery.dominio.adapter.ConsoleLogAdapter;
 //import com.mycompany.trabalho.delivery.dominio.model.pedido.Pedido;
 //import com.mycompany.trabalho.delivery.dominio.model.pedido.PedidoPendenteState;
@@ -39,5 +39,5 @@ public class MontarPedido {
 //        repositorioPedidos.salvarPedido(pedido);
 //        
 //        //System.out.println("Pedido " + pedido. + " foi criado com sucesso.");
-//    }
+//    
 }
