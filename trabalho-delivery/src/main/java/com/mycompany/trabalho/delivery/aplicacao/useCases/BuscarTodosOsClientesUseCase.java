6fce2ba559658617b6b1d2dbbcfc4423a25cf652
André Louzada <andretavareslouzada@gmail.com>
2026-02-13 15:56:45 -0300

    criacao de BuscarTodosOsClientesUseCase + implementacao dos DTOs de pedido + alteracao na cliente controller

--- trabalho-delivery/src/main/java/com/mycompany/trabalho/delivery/aplicacao/useCases/BuscarTodosOsClientesUseCase.java
+++ trabalho-delivery/src/main/java/com/mycompany/trabalho/delivery/aplicacao/useCases/BuscarTodosOsClientesUseCase.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.trabalho.delivery.aplicacao.useCases;
 
+import com.mycompany.trabalho.delivery.aplicacao.dto.CreateClienteOutputDTO;
 import com.mycompany.trabalho.delivery.dominio.model.cliente.Cliente;
 import com.mycompany.trabalho.delivery.dominio.port.IClienteRepository;
 import java.util.List;
@@ -18,8 +19,22 @@ public class BuscarTodosOsClientesUseCase {
     public BuscarTodosOsClientesUseCase(IClienteRepository repositorio) {
         this.repositorio = repositorio;
     }
-    public List<Cliente> executar() {
-        List<Cliente> lista = repositorio.buscarTodos();
-        return lista; 
+    public List<CreateClienteOutputDTO> executar() {
+
+        List<Cliente> clientes = repositorio.buscarTodos();
+
+        return clientes.stream().map(cliente -> {
+
+            CreateClienteOutputDTO dto = new CreateClienteOutputDTO();
+            dto.nome = cliente.getNome();
+            dto.email = cliente.getEmail();
+            dto.cidade = cliente.getCidade();
+            dto.bairro = cliente.getBairro();
+            dto.rua = cliente.getRua();
+            dto.numero = cliente.getNumero();
+
+            return dto;
+
+        }).toList();
     }
 }
--- trabalho-delivery/src/main/java/com/mycompany/trabalho/delivery/presentation/controllers/ClienteController.java
+++ trabalho-delivery/src/main/java/com/mycompany/trabalho/delivery/presentation/controllers/ClienteController.java
@@ -1,33 +1,42 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+ 
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template*/
 
 package com.mycompany.trabalho.delivery.presentation.controllers;
 
 import com.mycompany.trabalho.delivery.aplicacao.dto.CreateClienteInputDTO;
 import com.mycompany.trabalho.delivery.aplicacao.dto.CreateClienteOutputDTO;
+import com.mycompany.trabalho.delivery.aplicacao.useCases.BuscarTodosOsClientesUseCase;
 import com.mycompany.trabalho.delivery.aplicacao.useCases.CadastrarClienteUseCase;
+import com.mycompany.trabalho.delivery.presentation.ui.ClienteView;
+import java.util.List;
 import java.util.Optional;
 import javax.swing.JOptionPane;
 
-public class ClienteController {
-    
-    private final CadastrarClienteUseCase cadastrarCliente;
 
-    public ClienteController(CadastrarClienteUseCase cadastrar) {
-        this.cadastrarCliente = cadastrar;
+public class ClienteController {
+
+    private final CadastrarClienteUseCase cadastrarCliente;
+    private final BuscarTodosOsClientesUseCase listarClientes;
+
+    public ClienteController(
+            CadastrarClienteUseCase cadastrarCliente,
+            BuscarTodosOsClientesUseCase listarClientes
+    ) {
+        this.cadastrarCliente = cadastrarCliente;
+        this.listarClientes = listarClientes;
     }
 
-    
     public Optional<CreateClienteOutputDTO> salvar(CreateClienteInputDTO dados) {
         try {
             return cadastrarCliente.executar(dados);
         } catch (Exception e) {
-            //log
-            return null;
+            return Optional.empty(); 
         }
     }
-    
-    
+
+    public List<CreateClienteOutputDTO> listarTodos() {
+        return listarClientes.executar();
+    }
 }
